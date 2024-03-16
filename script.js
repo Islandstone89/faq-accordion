6a8372a69e699129e0767e5d53529514128b9a9c
Øystein Håberg <75746434+Islandstone89@users.noreply.github.com>
2024-03-16 22:42:12 +0100

    Add files via upload
    * Replaced inline svgs with <img>
    * Experimented with icon change on click

--- script.js
+++ script.js
@@ -3,11 +3,33 @@
 
 let disclosureButtons = document.querySelectorAll(".js-disclosure-btn");
 
+/* Select the icons */
+
+let icons = document.querySelectorAll(".icon");
+
+/* Create toggle function */
+
 function toggleDisclosure() {
     let isExpanded = this.getAttribute('aria-expanded') === 'true';
     this.setAttribute('aria-expanded', !isExpanded);
+    let icon = this.firstElementChild;
+    
+    if (icon.getAttribute("src", "assets/images/icon-plus.svg")) {
+        icon.setAttribute("src", "assets/images/icon-minus.svg")
+    }
+
+    else if (icon.getAttribute("src", "assets/images/icon-minus.svg")) {
+        icon.setAttribute("src", "assets/images/icon-plus.svg")
+    }
+    
+    else {
+        return;
+    }
 }
+
+/* Loop through the buttons, add event listener */
 
 disclosureButtons.forEach(button => {
     button.addEventListener("click", toggleDisclosure)
 });
+
